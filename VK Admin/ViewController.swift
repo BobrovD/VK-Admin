5609e7244fec3b58711580f0247b4b1572b55f73
Dmitry Bobrov <d.bobrov@corp.vk.com>
2016-11-07 21:20:35 +0300

    Initial Commit

---
+++ VK Admin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VK Admin
+//
+//  Created by Orange on 07.11.16.
+//  Copyright © 2016 Dmitry Bobrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
